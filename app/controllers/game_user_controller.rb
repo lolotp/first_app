4e651194b4534b0a062e96cedb46e48607ada4b4
Nguyen Phi Long <Zeka304@yahoo.com>
2013-02-24 20:36:19 +0800

    Implements game_user/register controll

--- app/controllers/game_user_controller.rb
+++ app/controllers/game_user_controller.rb
@@ -11,6 +11,18 @@ class GameUserController < ApplicationController
   end
 
   def register
+	user = User.new(:name => params[:name], :email => params[:email], :password => params[:password], :password_confirmation => params[:password_confirmation])
+	respond_to do |format|
+	  if user
+		if user.save
+		  format.html { render :text => "Successfully registered." }
+		else
+		  format.html { render :text => "Error: User already exists." }
+		end
+	  else
+		format.html { render :text => "Error: Failed to register." }
+	  end
+	end
   end
 
   def games
