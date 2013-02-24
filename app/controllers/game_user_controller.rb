82cf1ca238b0609e188d66e40cffb2f91e3858b1
lolotp <lolo_vn@yahoo.co.uk>
2013-02-24 20:37:22 +0800

    implemented games and gamestatus

--- app/controllers/game_user_controller.rb
+++ app/controllers/game_user_controller.rb
@@ -1,11 +1,26 @@
 class GameUserController < ApplicationController
+  def get_user_id_from_security_token(token)
+    begin
+        tokens = token.split
+        id = Integer(tokens[0])
+        u = User.find_by_id(id)
+        if (tokens[1] == u[:password_digest])                
+            u[:id]
+        else
+            -1
+        end
+    rescue ArgumentError
+        -1
+    end 
+  end
+  
   def login
     user = User.find_by_email(params[:email].downcase)
     respond_to do |format|
       if user && user.authenticate(params[:password])
-               format.html { render :text => user[:id].to_s()+" "+user[:password_digest] }
+        format.html { render :text => user[:id].to_s()+" "+user[:password_digest] }
       else
-        format.html { render :text => "Invalid email/password"}
+        format.html { render :text => "Error: Invalid email/password", :status => 401}
       end
     end
   end
@@ -14,9 +29,30 @@ class GameUserController < ApplicationController
   end
 
   def games
+    @user = User.find_by_id( get_user_id_from_security_token (params[:security_token]) )
+    respond_to do |format|
+        if @user
+            format.html { render :json => @user.games }
+        else
+            format.html { render :text => "Error: Invalid security token", :status => 401}
+        end
+    end
   end
 
   def game_status
+    user = User.find_by_id( get_user_id_from_security_token (params[:security_token]) )
+    respond_to do |format|
+        if (user)
+            game = user.games.find_by_id( params[:game_id] )
+            if (game)
+                format.json { render :json => game.locations }
+            else
+                format.html { render :text => "Error: Invalid game id", :status => 401}
+            end
+        else
+            format.html { render :text => "Error: Invalid security token", :status => 401}
+        end
+    end
   end
 
   def request_clue
